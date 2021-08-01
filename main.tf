dacbbd124a1db67eb3feecad320c60b1845e17f3
viv-garot <vivien@hashicorp.com>
2021-08-01 21:56:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+  }
+}
+
 resource "aws_instance" "webserver"{
   instance_type = "t2.micro"
   ami = "ami-05fa05752fc432eeb"
